b2e550f2bc6096f470f4583814d137a5824e7012
Luis Felipe Cortes <lfe.cortes@gmail.com>
2016-11-21 13:50:42 -0200

    rails new

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,16 @@
+class CoachingController < ApplicationController
+  def answer
+    query = params[:query]
+
+    if query == "i am going to work right now!" || query == "I'AM GOING TO WORK RIGHT NOW"
+      @answer = "OK"
+    elsif query.slice(-1) == "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def ask
+  end
+end
